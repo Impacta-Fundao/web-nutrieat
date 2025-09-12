c25f96dc2c1a2da123e5df4eef85733987225b58
Pedro Rogel <pedrorogel3@gmail.com>
2025-09-12 18:51:21 -0300

    feat: add props to produtos view

--- src/models/produtos/types/produtos-props-model.ts
+++ src/models/produtos/types/produtos-props-model.ts
@@ -1,23 +1,38 @@
-export interface ProdutosModel {
-    id: number 
-    nome: string | null
-    preco: number | null
+import {
+  FieldErrors,
+  SubmitHandler,
+  UseFormHandleSubmit,
+  UseFormRegister,
+  UseFormReset,
+} from "react-hook-form";
+
+export interface ProdutosModelData {
+  id: number;
+  nome: string;
+  preço: string;
 }
 
 export interface ApiResponse {
-  data: ProdutosModel[];
+  data: { data: ProdutosModelData[] };
 }
 
-export interface ApiResponseId{
-    data: ProdutosModel;
+export interface ApiResponseId {
+  data: ProdutosModelData;
 }
 
-export interface AdministrationViewProps {
-  data: ProdutosModel[];
+export interface ProdutoFormData {
+  nome: string;
+  preco: string;
+}
+
+export interface ProdutoViewProps {
+  data: ProdutosModelData[];
   loading: boolean;
-}
-
-export interface ProdutoData {
-  nome: string
-  preco: number | string
+  produto: ProdutosModelData | null | undefined;
+  register: UseFormRegister<ProdutoFormData>;
+  handleSubmit: UseFormHandleSubmit<ProdutoFormData, ProdutoFormData>;
+  onSubmit: SubmitHandler<ProdutoFormData>;
+  errors: FieldErrors<ProdutoFormData>;
+  reset: UseFormReset<ProdutoFormData>;
+  error: string | null;
 }
