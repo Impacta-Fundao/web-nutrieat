af73de2a294a11831f2d28de0b456d4de595d2fb
Pedro Rogel <pedrorogel3@gmail.com>
2025-10-03 11:33:54 -0300

    refactor: add component work in progress to modules that to come

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,9 @@
 import WorkInProgress from "@/components/ui/animation/work-in-progress";
-import Produtos from "@/ui/Products";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col justify-between items-center gap-5 p-3">
+     
       <WorkInProgress />
     </div>
   );
